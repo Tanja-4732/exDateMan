f7569651a76c31a8526067cf9fb41f145d1685b7
Bernd <bernd.lafer@gmail.com>
2018-09-05 19:48:31 +0200

    Things component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThingCardComponent } from './thing-card/thing-card.component';
 import { StockCardComponent } from './stock-card/stock-card.component';
+import { ThingsComponent } from './things/things.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ThingCardComponent,
-    StockCardComponent
+    StockCardComponent,
+    ThingsComponent
   ],
   imports: [
     BrowserModule,
