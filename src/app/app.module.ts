28d4c32c4fe5d3078dfc09e12a67f8e941427921
Bernd <bernd.lafer@gmail.com>
2018-09-05 17:41:45 +0200

    Material beginnings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { ThingCardComponent } from './thing-card/thing-card.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    ThingCardComponent,
+    StockCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
