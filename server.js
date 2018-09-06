76e40d25167fa6232721df09f46dcab946c99e01
Bernd <bernd.lafer@gmail.com>
2018-09-06 18:35:15 +0200

    Preparing for Heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + '/dist/exDateMan')));
+app.use(express.static(__dirname + '/dist/exDateMan'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/exDateMan/index.html'));
-})
+  res.sendFile(path.join(__dirname + '/dist/exDateMan/index.html'))
+});
 
 app.listen(process.env.PORT || 8080);
