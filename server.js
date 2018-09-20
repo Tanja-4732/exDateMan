2cb75c71afd3ca890da7cb36a9edd0da582553bb
Bernd L <bernd.lafer@gmail.com>
2018-09-20 18:03:30 +0200

    API implementation started

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist/exDateMan"));
 
+app.get("/api", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.sendFile(path.join(__dirname + "/dist/exDateMan/index.html"));
+  res.end('Hello World');
+});
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/dist/exDateMan/index.html"))
 });
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { ThingsComponent } from "./things/things.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/things", pathMatch: "full" },
   { path: "things", component: ThingsComponent },
   { path: "addThing", component: AddThingComponent },
   { path: "thing/:thingName", component: StocksComponent },
   { path: "thing/:thingName/edit-thing", component: EditThingComponent },
   { path: "thing/:thingName/add-stock", component: AddStockComponent },
   { path: "thing/:thingName/stock/:stockId", component: EditStockComponent },
+  { path: "", redirectTo: "/things", pathMatch: "full" },
 ];
 
 @NgModule({
