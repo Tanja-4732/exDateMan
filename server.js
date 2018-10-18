cecc79c5ee6374b79417c300e165707e848661d1
Bernd L <bernd.lafer@gmail.com>
2018-10-18 17:00:12 +0200

    Can receive a POST request

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ const path = require("path");
 
 const app = express();
 
+var bodyParser = require('body-parser');
+
+var rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+}
+
+app.use(bodyParser.json({
+  verify: rawBodySaver
+}));
+
+app.use(bodyParser.urlencoded({
+  verify: rawBodySaver,
+  extended: true
+}));
+
+app.use(bodyParser.raw({
+  verify: rawBodySaver,
+  type: function () {
+    return true
+  }
+}));
+
+
 app.use(express.static(__dirname + "/dist/exDateMan"));
 
 app.get("/api/v1/things", (req, res, next) => {
@@ -13,7 +38,7 @@ app.get("/api/v1/things", (req, res, next) => {
 });
 
 app.post("/api/v1/thing", (req, res, next) => {
-  console.log(req.body + "");
+  console.log("Body: " + req.rawBody);
   res.statusCode = 200;
 
 })
