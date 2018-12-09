14300c6b237b2e9ecb49389ba080a20e9dd0e214
Bernd <bernd.lafer@gmail.com>
2018-12-09 20:48:56 +0100

    Working TS implementation beginings

--- server.ts
+++ server.ts
@@ -1,32 +1,6 @@
-// import "babel-polyfill";
+import app from "./server/app";
+const PORT = process.env.PORT || 420;
 
-import * as path from "path";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-
-import routes from "./routes";
-import config from "./config/config";
-
-
-
-
-
-
-
-
-
-
-
-// TODO Give next a type
-app.get("/api", (req: Request, res: Response, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  // res.sendFile(path.join(__dirname + "/dist/exDateMan/index.html"));
-  res.end("Hello World");
+app.listen(PORT, () => {
+    console.log('Express server listening on port ' + PORT);
 });
-
-app.get("/*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname + "/dist/exDateMan/index.html"));
-});
-
-app.listen(process.env.PORT || 420);
