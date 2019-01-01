94b72764bef5b2cf25233f858ab184fb7b32ed91
Bernd <bernd.lafer@gmail.com>
2019-01-01 22:48:55 +0100

    Database credentials can now be read from env vars

--- queries.js
+++ queries.js
@@ -10,11 +10,11 @@ var options = {
 
 var pgp = require('pg-promise')(options);
 const db = pgp({
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: process.env.DB,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.HP_HOST,
+  port: process.env.HP_PORT,
+  database: process.env.HP_DB,
+  user: process.env.HP_USER,
+  password: process.env.HP_PWD,
   ssl: true
 });
 
