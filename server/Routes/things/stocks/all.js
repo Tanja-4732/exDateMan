143f3df25d6ec65733448c26dd322714195cd9de
Bernd <bernd.lafer@gmail.com>
2019-01-05 21:01:48 +0100

    Pruning JS files

--- server/Routes/things/stocks/all.js
+++
@@ -1,8 +0,0 @@
-const data = require('../../../data.json');
-
-module.exports = (req, res) => {
-  const modelId = req.params.modelId * 1;
-  const cars = data.cars.filter(c => c.modelId === modelId);
-
-  res.status(200).json({ cars });
-};
