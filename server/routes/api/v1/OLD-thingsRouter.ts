48f83ce8e8459a8237a7cc855ea8f64b7e4f45e8
Bernd L <bernd.lafer@gmail.com>
2019-01-14 13:46:16 +0100

    Fix folder structure

--- server/routes/api/v1/OLD-thingsRouter.ts
+++ server/routes/api/v1/OLD-thingsRouter.ts
@@ -5,19 +5,20 @@ import v1Routes from "./stocksRouter";
 
 export class ThingsRouter {
   public thingsRoutes: Router = Router();
-  constructor (private inventoryId: number) {
-
+  constructor(private inventoryId: number) {
     // Security
-    this.thingsRoutes.use("/", (req: Request, res: Response, next: NextFunction) => {
-      log("So secure.");
-      next();
-    });
+    this.thingsRoutes.use(
+      "/",
+      (req: Request, res: Response, next: NextFunction) => {
+        log("So secure.");
+        next();
+      }
+    );
 
     // Return all things
     this.thingsRoutes.get("/", (req: Request, res: Response) => {
-  log("inventoryId=" + req.params.inventoryId);
-req.
-      res.status(200).json({
+      log("inventoryId=" + req.params.inventoryId);
+      req.res.status(200).json({
         message: "These are all the things " + req.params.inventoryId
       });
     });
@@ -27,9 +28,7 @@ req.
       // TODO
     });
 
-
     this.thingsRoutes.use("/v1", v1Routes);
-
   }
 }
 // export default thingsRouter.thingsRoutes;
