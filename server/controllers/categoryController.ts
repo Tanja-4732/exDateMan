4339570be242173700c71e22e8355b1b5bd3344e
Bernd L <bernd.lafer@gmail.com>
2019-02-07 07:37:49 +0100

    Update category

--- server/controllers/categoryController.ts
+++ server/controllers/categoryController.ts
@@ -7,8 +7,8 @@ import { Inventory } from "../models/inventoryModel";
 
 interface CategoryRequest {
   name: string;
-  parent: Category; // initially number
-  children: Category[]; // initially number[]
+  parent: number;
+  children: number[];
 }
 
 export default class CategoryController {
@@ -46,6 +46,7 @@ export default class CategoryController {
         status: 500,
         error: "Something went wrong server-side."
       });
+      return;
     }
     res.status(200).json({
       status: 200,
@@ -62,19 +63,57 @@ export default class CategoryController {
         res.locals.actingUser,
         res.locals.inventory as Inventory,
         InventoryUserAccessRightsEnum.WRITE
-      ))
-    ) {
-      res.status(403).json({
-        status: 403,
-        error:
-          "Requestor doesn't have the WRITE role or higher for this inventory."
+        ))
+        ) {
+          res.status(403).json({
+            status: 403,
+            error:
+            "Requestor doesn't have the WRITE role or higher for this inventory."
+          });
+          return;
+        }
+
+    // Get the category to be updated
+    const catToUpdate: Category = res.locals.category;
+
+    // Get the data needed from req.body
+    let parent: Category;
+    const children: Category[] = [];
+    try {
+      parent = await this.getCategoryByIdOrFail((req.body as CategoryRequest).parent);
+      for (const catId of req.body.children) {
+        children.push(await this.getCategoryByIdOrFail(catId));
+      }
+    } catch (err) {
+      res.status(404).json({
+        status: 404,
+        error: "The requested categories couldn't be found"
       });
       return;
     }
 
+    // Set the requested values
+    catToUpdate.parentCategory = parent;
+    catToUpdate.childCategories = children;
+    catToUpdate.name = (req.body as CategoryRequest).name;
+
+    // Save the data
+    try {
+      entityManager.save(catToUpdate);
+    } catch (err) {
+      res.status(500).json({
+        status: 500,
+        error: "Something went wrong server-side"
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      message: "Updated category"
+    });
   }
 
-  static createCategory(req: Request, res: Response): any {
+  static async createCategory(req: Request, res: Response): Promise<any> {
     const entityManager: EntityManager = getManager();
 
     // Check for authorization
