f32c3451f0b9da15241998541c3f2ee1dcecee2e
Bernd <bernd.lafer@gmail.com>
2019-02-19 19:16:38 +0100

    Prepare for buildpack

--- src/app/services/inventory/inventory-service.service.ts
+++ src/app/services/inventory/inventory-service.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Inventory } from '../../models/inventory';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getInventories(): Inventory[] {
     return []; // TODO implement mock or real API client
