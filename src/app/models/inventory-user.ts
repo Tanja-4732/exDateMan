eeb48f46231d18f5effd8527c4e8839806009f52
Bernd <bernd.lafer@gmail.com>
2019-02-21 00:21:32 +0100

    User model added (client)

--- src/app/models/inventory-user.ts
+++ src/app/models/inventory-user.ts
@@ -1,5 +1,6 @@
 import { Inventory } from "./inventory";
 import { InventoryUserAccess } from "./inventory-user-access.enum";
+import { User } from "./user";
 
 export class InventoryUser {
   inventory: Inventory;
