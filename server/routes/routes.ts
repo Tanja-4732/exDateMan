75bcd50a2c4e55a1e77972b9a9217d2c4640cf1b
Bernd <bernd.lafer@gmail.com>
2019-02-24 22:18:47 +0100

    Merge #13 again

---
+++ server/routes/routes.ts
@@ -0,0 +1,28 @@
+import * as path from "path";
+import { log } from 'util';
+import { Request, Response, Application, Router, NextFunction } from "express";
+
+import apiRoutes from "./api/apiRoutes";
+
+const routes: Router = Router();
+
+// API routes
+routes.use("/api/", apiRoutes);
+
+// Serve main page
+routes.use((req: Request, res: Response, next: NextFunction) => {
+  // res.redirect("/");
+  // log("Triggered fallback");
+  log(  req.originalUrl);
+  log("serve main page");
+  res
+      .status(200)
+      .sendFile(
+        "dist/exDateMan/index.html",
+        {root: process.env.EDM_ROOT_PATH}
+      );
+
+  // next();
+});
+
+export default routes;
