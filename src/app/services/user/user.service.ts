f118f6871928d9a0e875ed3f81181cc3245ad220
Bernd L <bernd.lafer@gmail.com>
2019-03-04 14:31:58 +0100

    Cllient-side getUser implemented

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../models/user/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClientl) { }
+
+  async getUser(email: string): Promise<User> {
+    const qRes: User = await this.http.get<User>(this.baseUrl + "/users/" + email);
+    return qRes;
+  }
+}
