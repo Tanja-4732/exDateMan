4901044783ecdf9e2f024fa343a20c9f2cb678fa
Bernd <18219148+Bernd-L@users.noreply.github.com>
2019-06-25 21:34:02 +0200

    Testing scanner

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,8 @@
 
 import { log } from "util";
 import "source-map-support/register";
+import { createServer } from "https";
+import { readFileSync } from "fs";
 
 // Set port
 const PORT: string = process.env.PORT || 420 + "";
@@ -18,8 +20,25 @@ log("Starting server in " + process.env.EDM_WORKING_DIRECTORY);
 
 // Import the app
 import app from "./app";
+import { resolve } from "path";
 
-// Log listening
-app.listen(PORT, () => {
-  log("Server listening on port " + PORT);
-});
+// Check for development mode
+if (process.env.EDM_MODE !== "development") {
+  // Don't use SSL and let the host worry about that
+  app.listen(PORT, () => {
+    log("Server listening on port " + PORT);
+  });
+} else {
+  // Use self-signed SSL certificate for development
+  createServer(
+    {
+      key: readFileSync(resolve(__dirname, "../../backend/keys/server.key")),
+      // key: readFileSync(resolve(__dirname, "../../backend/keys/private.key")),
+      cert: readFileSync(resolve(__dirname, "../../backend/keys/server.crt")),
+      passphrase: "1234" // As secure as it gets
+    },
+    app
+  ).listen(PORT, () => {
+    log("SSL server listening on port " + PORT);
+  });
+}
