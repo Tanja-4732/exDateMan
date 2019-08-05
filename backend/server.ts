7887b27adca4c28ba91e94110452d0fdfa308208
Bernd-L <18219148+Bernd-L@users.noreply.github.com>
2019-08-05 23:43:06 +0200

    SSL works; fix #88

--- backend/server.ts
+++ backend/server.ts
@@ -8,9 +8,11 @@ import { log } from "util";
 import "source-map-support/register";
 import { createServer } from "https";
 import { readFileSync } from "fs";
+import * as express from "express";
 
-// Set port
-const PORT: string = process.env.PORT || 4242 + "";
+// Set the ports
+const PORT: string = process.env.PORT || 443 + "";
+const INSECURE_PORT: string = process.env.INSECURE_PORT || 80 + "";
 
 // Set EDM_WORKING_DIRECTORY (working directory of the server)
 process.env.EDM_WORKING_DIRECTORY = __dirname;
@@ -25,37 +27,42 @@ import { resolve } from "path";
 // Check for development mode
 switch (process.env.EDM_SSL) {
   case "yes":
-
     const PUBLIC_KEY: string | Buffer =
-      process.env.EDM_PUBLIC_KEY_VAL || readFileSync(process.env.EDM_PUBLIC_KEY);
-
-
+      process.env.EDM_PUBLIC_KEY_VAL ||
+      readFileSync(process.env.EDM_PUBLIC_KEY);
 
     // Certificate
-    const privateKey = readFileSync(process.env.EDM_, 'utf8');
-    const certificate = readFileSync(process.env.EDM_, 'utf8');
-    const ca = readFileSync(process.env.EDM_CA, 'utf8');
+    const privateKey: string =
+      process.env.EDM_PK_VAL || readFileSync(process.env.EDM_PK, "utf8");
+    const certificate: string =
+      process.env.EDM_CERT_VAL || readFileSync(process.env.EDM_CERT, "utf8");
+    const ca: string =
+      process.env.EDM_CA_VAL || readFileSync(process.env.EDM_CA, "utf8");
 
+    const credentials = {
+      key: privateKey,
+      cert: certificate,
+      ca
+    };
 
-
-    // Use self-signed SSL certificate for development
-    createServer(
-      {
-        key: readFileSync(resolve(__dirname, "../../backend/keys/server.key")),
-        // key: readFileSync(resolve(__dirname, "../../backend/keys/private.key")),
-        cert: readFileSync(resolve(__dirname, "../../backend/keys/server.crt")),
-        passphrase: "1234" // As secure as it gets
-      },
-      app
-    ).listen(PORT, () => {
-      log("HTTPS server listening on port " + PORT);
+    // Create and start the https app server
+    createServer(credentials, app).listen(PORT, () => {
+      log("HTTPS app server listening on port " + PORT);
     });
+
+    // Create and start the http redirect server
+    express()
+      .use("*", (req, res) => {
+        res.redirect("https://" + req.headers.host + req.url);
+      })
+      .listen(INSECURE_PORT, () => {
+        log("HTTP redirect server listening on port " + INSECURE_PORT);
+      });
     break;
+
   default:
     // Don't use SSL
-    app.listen(PORT, () => {
-      log("HTTP server listening on port " + PORT);
+    app.listen(process.env.PORT || 80 + "", () => {
+      log("HTTP app server listening on port " + process.env.PORT || 80 + "");
     });
 }
-
-log("Server closed");
--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,4 +1,6 @@
-export const environment = {
+import { Environment } from "./environment.d";
+
+export const environment: Environment = {
   production: true,
   baseUrl: "https://example.com/api/v1" // don't append a slash
 };
