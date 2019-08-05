88f0f24f1b3d0796fbb54c373c9f74cda967714e
Bernd-L <18219148+Bernd-L@users.noreply.github.com>
2019-08-05 18:28:19 +0200

    [ci-skip] ssl progress

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ import { createServer } from "https";
 import { readFileSync } from "fs";
 
 // Set port
-const PORT: string = process.env.PORT || 420 + "";
+const PORT: string = process.env.PORT || 4242 + "";
 
 // Set EDM_WORKING_DIRECTORY (working directory of the server)
 process.env.EDM_WORKING_DIRECTORY = __dirname;
@@ -23,22 +23,39 @@ import app from "./app";
 import { resolve } from "path";
 
 // Check for development mode
-if (true || process.env.EDM_MODE !== "development") {
-  // Don't use SSL and let the host worry about that
-  app.listen(PORT, () => {
-    log("Server listening on port " + PORT);
-  });
-} else {
-  // Use self-signed SSL certificate for development
-  createServer(
-    {
-      key: readFileSync(resolve(__dirname, "../../backend/keys/server.key")),
-      // key: readFileSync(resolve(__dirname, "../../backend/keys/private.key")),
-      cert: readFileSync(resolve(__dirname, "../../backend/keys/server.crt")),
-      passphrase: "1234" // As secure as it gets
-    },
-    app
-  ).listen(PORT, () => {
-    log("SSL server listening on port " + PORT);
-  });
+switch (process.env.EDM_SSL) {
+  case "yes":
+
+    const PUBLIC_KEY: string | Buffer =
+      process.env.EDM_PUBLIC_KEY_VAL || readFileSync(process.env.EDM_PUBLIC_KEY);
+
+
+
+    // Certificate
+    const privateKey = readFileSync(process.env.EDM_, 'utf8');
+    const certificate = readFileSync(process.env.EDM_, 'utf8');
+    const ca = readFileSync(process.env.EDM_CA, 'utf8');
+
+
+
+    // Use self-signed SSL certificate for development
+    createServer(
+      {
+        key: readFileSync(resolve(__dirname, "../../backend/keys/server.key")),
+        // key: readFileSync(resolve(__dirname, "../../backend/keys/private.key")),
+        cert: readFileSync(resolve(__dirname, "../../backend/keys/server.crt")),
+        passphrase: "1234" // As secure as it gets
+      },
+      app
+    ).listen(PORT, () => {
+      log("HTTPS server listening on port " + PORT);
+    });
+    break;
+  default:
+    // Don't use SSL
+    app.listen(PORT, () => {
+      log("HTTP server listening on port " + PORT);
+    });
 }
+
+log("Server closed");
