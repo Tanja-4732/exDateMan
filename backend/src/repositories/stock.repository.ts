111bda59c01b78839e6cb68b33e4741008f95be1
Bernd-L <18219148+Bernd-L@users.noreply.github.com>
2019-08-14 04:10:42 +0200

    Revert "Trying to get relations to work"
    This reverts commit 9d95ea1e5a1600d12622db05a274877f55b16bb6.

--- backend/src/repositories/stock.repository.ts
+++ backend/src/repositories/stock.repository.ts
@@ -1,5 +1,5 @@
-import {DefaultCrudRepository, Options, Filter} from '@loopback/repository';
-import {Stock, StockRelations, StockWithRelations} from '../models';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {Stock, StockRelations} from '../models';
 import {PgDataSource} from '../datasources';
 import {inject} from '@loopback/core';
 
@@ -8,56 +8,9 @@ export class StockRepository extends DefaultCrudRepository<
   typeof Stock.prototype.id,
   StockRelations
 > {
-  constructor(@inject('datasources.pg') dataSource: PgDataSource) {
+  constructor(
+    @inject('datasources.pg') dataSource: PgDataSource,
+  ) {
     super(Stock, dataSource);
-  }
-
-  async find(
-    filter?: Filter<Stock>,
-    options?: Options,
-  ): Promise<StockWithRelations[]> {
-    // Prevent juggler for applying "include" filter
-    // Juggler is not aware of LB4 relations
-    const include = filter && filter.include;
-    filter = {...filter, include: undefined};
-
-    const result = await super.find(filter, options);
-
-    // poor-mans inclusion resolver, this should be handled by DefaultCrudRepo
-    // and use `inq` operator to fetch related Stock-lists in fewer DB queries
-    // this is a temporary implementation, please see
-    // https://github.com/strongloop/loopback-next/issues/3195
-    if (include && include.length && include[0].relation === 'thing') {
-      await Promise.all(
-        result.map(async r => {
-          r.thing = await this.thing(r.id);
-        }),
-      );
-    }
-
-    return result;
-  }
-
-  async findById(
-    id: typeof Stock.prototype.id,
-    filter?: Filter<Stock>,
-    options?: Options,
-  ): Promise<StockWithRelations> {
-    // Prevent juggler for applying "include" filter
-    // Juggler is not aware of LB4 relations
-    const include = filter && filter.include;
-    filter = {...filter, include: undefined};
-
-    const result = await super.findById(id, filter, options);
-
-    // poor-mans inclusion resolver, this should be handled by DefaultCrudRepo
-    // and use `inq` operator to fetch related Stock-lists in fewer DB queries
-    // this is a temporary implementation, please see
-    // https://github.com/strongloop/loopback-next/issues/3195
-    if (include && include.length && include[0].relation === 'thing') {
-      result.thing = await this.thing(result.id);
-    }
-
-    return result;
   }
 }
--- backend/src/repositories/thing.repository.ts
+++ backend/src/repositories/thing.repository.ts
@@ -1,86 +1,14 @@
-import {
-  DefaultCrudRepository,
-  HasManyRepositoryFactory,
-  repository,
-} from '@loopback/repository';
-import {Thing, ThingRelations, Stock, ThingWithRelations} from '../models';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {Thing, ThingRelations} from '../models';
 import {PgDataSource} from '../datasources';
-import {inject, Getter} from '@loopback/core';
-import {StockRepository} from './stock.repository';
-
-import {Filter, Options} from '@loopback/repository';
-import {StockWithRelations} from '../models';
+import {inject} from '@loopback/core';
 
 export class ThingRepository extends DefaultCrudRepository<
   Thing,
   typeof Thing.prototype.id,
   ThingRelations
 > {
-  // Declare a HasManyRepositoryFactory here
-  public readonly stocks: HasManyRepositoryFactory<
-    Stock,
-    typeof Thing.prototype.id
-  >;
-
-  constructor(
-    @inject('datasources.pg') dataSource: PgDataSource,
-    // Inject StockRepository-getter
-    @repository.getter(StockRepository)
-    protected stockRepositoryGetter: Getter<StockRepository>,
-  ) {
+  constructor(@inject('datasources.pg') dataSource: PgDataSource) {
     super(Thing, dataSource);
-    // Some kind of factory
-    this.stocks = this.createHasManyRepositoryFactoryFor(
-      'stocks',
-      stockRepositoryGetter,
-    );
-  }
-
-  async find(
-    filter?: Filter<Thing>,
-    options?: Options,
-  ): Promise<ThingWithRelations[]> {
-    // Prevent juggler for applying "include" filter
-    // Juggler is not aware of LB4 relations
-    const include = filter && filter.include;
-    filter = {...filter, include: undefined};
-    const result = await super.find(filter, options);
-
-    // poor-mans inclusion resolver, this should be handled by DefaultCrudRepo
-    // and use `inq` operator to fetch related stocks in fewer DB queries
-    // this is a temporary implementation, please see
-    // https://github.com/strongloop/loopback-next/issues/3195
-    if (include && include.length && include[0].relation === 'stocks') {
-      await Promise.all(
-        result.map(async r => {
-          r.stocks = await this.stocks(r.id).find();
-        }),
-      );
-    }
-
-    return result;
-  }
-
-  async findById(
-    id: typeof Thing.prototype.id,
-    filter?: Filter<Thing>,
-    options?: Options,
-  ): Promise<ThingWithRelations> {
-    // Prevent juggler for applying "include" filter
-    // Juggler is not aware of LB4 relations
-    const include = filter && filter.include;
-    filter = {...filter, include: undefined};
-
-    const result = await super.findById(id, filter, options);
-
-    // poor-mans inclusion resolver, this should be handled by DefaultCrudRepo
-    // and use `inq` operator to fetch related stocks in fewer DB queries
-    // this is a temporary implementation, please see
-    // https://github.com/strongloop/loopback-next/issues/3195
-    if (include && include.length && include[0].relation === 'stocks') {
-      result.stocks = await this.stocks(result.id).find();
-    }
-
-    return result;
   }
 }
