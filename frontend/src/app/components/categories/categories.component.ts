71b8d39feb68a93989d4c8507547dc1f2ec4bfb8
Bernd-L <git@bernd.pw>
2020-02-24 14:48:15 +0100

    Example MatTree in UI

--- frontend/src/app/components/categories/categories.component.ts
+++ frontend/src/app/components/categories/categories.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { CategoryService } from "src/app/services/category/category.service";
 
-import { FlatTreeControl } from "@angular/cdk/tree";
+import { FlatTreeControl, NestedTreeControl } from "@angular/cdk/tree";
 import {
   MatTreeFlatDataSource,
-  MatTreeFlattener
+  MatTreeFlattener,
+  MatTreeNestedDataSource
 } from "@angular/material/tree";
 import {
   CrumbTrailComponent,
@@ -15,18 +16,55 @@ import { Inventory } from "src/app/models/inventory/inventory";
 import { InventoryService } from "src/app/services/inventory/inventory.service";
 import { v4 } from "uuid";
 
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: "Fruit",
+    children: [{ name: "Apple" }, { name: "Banana" }, { name: "Fruit loops" }]
+  },
+  {
+    name: "Vegetables",
+    children: [
+      {
+        name: "Green",
+        children: [{ name: "Broccoli" }, { name: "Brussels sprouts" }]
+      },
+      {
+        name: "Orange",
+        children: [{ name: "Pumpkins" }, { name: "Carrots" }]
+      }
+    ]
+  }
+];
+
 @Component({
   selector: "app-categories",
   templateUrl: "./categories.component.html",
   styleUrls: ["./categories.component.scss"]
 })
 export class CategoriesComponent implements OnInit {
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
   constructor(
     private cs: CategoryService,
     private is: InventoryService,
     private route: ActivatedRoute,
     private router: Router
-  ) {}
+  ) {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) =>
+    !!node.children && node.children.length > 0;
 
   name: string;
   inventoryUuid: string;
